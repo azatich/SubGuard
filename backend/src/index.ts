fc4e1d7bd285cac3bb7516f81670f2acfb1263d7
Azat <azatabdirashituly@gmail.com>
2026-04-08 07:07:51 +0500

    fix: fix send notification

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ const supabase = connectDb(
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", 'https://subsguard.vercel.app'],
     credentials: true,
   }),
 );
