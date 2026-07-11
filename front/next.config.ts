3b7a949cfee7a04c81cf055ad650451efa12193c
Azat <azatabdirashituly@gmail.com>
2026-07-12 01:35:32 +0500

    changed favicon

--- front/next.config.ts
+++ front/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL || "https://subguard.onrender.com/api"}/:path*`, 
+        destination: `${process.env.NEXT_PUBLIC_API_URL || "https://subguard-api.duckdns.org/api"}/:path*`, 
       },
     ];
   },
